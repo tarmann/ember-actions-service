750861eea07254170c27fc931a566013cf26d75d
Bruno Tarmann <bruno@tarmann.com.br>
2018-01-26 12:17:18 +0000

    Include sample task on the actions service

--- blueprints/actions-service/files/__root__/__path__/__name__-actions.js
+++ blueprints/actions-service/files/__root__/__path__/__name__-actions.js
@@ -3,6 +3,18 @@ import ActionsService from '../services/actions-service';
 
 export default ActionsService.extend({
 
-  resource: '<%= dasherizedModuleName %>'
+  resource: '<%= dasherizedModuleName %>',
+
+  /**
+   * Sample
+   * @param {object} store Ember store service
+   * @param {string} callback Callback name
+   * @param {object} model Resource model
+   */
+
+  // sampleTask: task(function * (store, callback, model){
+  //   yield model;
+  //   return { callback, model };
+  // })
 
 });
