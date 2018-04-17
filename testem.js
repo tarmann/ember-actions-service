59f5c57687e6d6a3f00f7b0477c3344ac1cd559e
Bruno Tarmann <bruno@tarmann.com.br>
2018-04-17 16:03:57 +0100

    Disable sandbox for CI

--- testem.js
+++ testem.js
@@ -12,6 +12,7 @@ module.exports = {
     Chrome: {
       mode: 'ci',
       args: [
+        '--disable-sandbox',
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=9222',
