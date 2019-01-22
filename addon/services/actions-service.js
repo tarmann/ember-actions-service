9a549b83a3994b8ee86b90a92b726834bdf35ad2
Liam Baker <liam.baker@storyful.com>
2019-01-22 12:22:19 +0000

    get(this -> this.get( where applicable

--- addon/services/actions-service.js
+++ addon/services/actions-service.js
@@ -14,13 +14,13 @@ export default Service.extend({
   afterSend(){},
 
   send(store, model, action, ...options){
-    const task = get(this, `${action}Task`);
-    const callback = `on${capitalize(action)}${capitalize(camelize(get(this, 'resource')))}`;
+    const task = this.get(`${action}Task`);
+    const callback = `on${capitalize(action)}${capitalize(camelize(this.get('resource')))}`;
 
-    assert(`Invalid actions-service resource name.`, !isEmpty( get(this, 'resource') ));
-    assert(`Store invalid for ${get(this, 'resource')} actions-service.`, typeOf(action) === 'string');
-    assert(`Must provide a valid action for ${get(this, 'resource')}.`,  typeOf(action) === 'string');
-    assert(`Task ${action} not found for ${get(this, 'resource')}.`, task);
+    assert(`Invalid actions-service resource name.`, !isEmpty( this.get('resource') ));
+    assert(`Store invalid for ${this.get('resource')} actions-service.`, typeOf(action) === 'string');
+    assert(`Must provide a valid action for ${this.get('resource')}.`,  typeOf(action) === 'string');
+    assert(`Task ${action} not found for ${this.get('resource')}.`, task);
 
     this.beforeSend(...arguments);
 
@@ -28,34 +28,34 @@ export default Service.extend({
   },
 
   findTask: task(function * (store, callback, model, id){
-    model = yield store.findRecord( get(this, 'resource'), id );
+    model = yield store.findRecord( this.get('resource'), id );
     return { callback, model };
   }),
 
   findAllTask: task(function * (store, callback){
-    const model = yield store.findAll( get(this, 'resource') );
+    const model = yield store.findAll( this.get('resource') );
     return { callback, model };
   }),
 
   createTask: task(function * (store, callback, _model, attrs){
-    const model = yield store.createRecord( get(this, 'resource'), attrs || {} );
+    const model = yield store.createRecord( this.get('resource'), attrs || {} );
     return { callback, model };
   }),
 
   saveTask: task(function * (store, callback, _model){
-    const model = _model.save ? _model : store.peekRecord( get(this, 'resource'), get(_model, 'id') );
+    const model = _model.save ? _model : store.peekRecord( this.get('resource'), get(_model, 'id') );
     yield model.save();
     return { callback, model };
   }),
 
   createAndSaveTask: task(function * (store, callback, _model, attrs){
-    const model = yield store.createRecord( get(this, 'resource'), attrs || {} );
+    const model = yield store.createRecord( this.get('resource'), attrs || {} );
     yield model.save();
     return { callback, model };
   }),
 
   deleteTask: task(function * (store, callback, _model){
-    const model = _model.destroyRecord ? _model : store.peekRecord( get(this, 'resource'), get(_model, 'id') );
+    const model = _model.destroyRecord ? _model : store.peekRecord( this.get('resource'), get(_model, 'id') );
     yield model.destroyRecord();
     return { callback, model };
   }),
